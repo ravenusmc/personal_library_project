0a8c8942b1d16bc934f5dc6c91ec5e79ae2cfc23
ravenusmc <mcuddy77@gmail.com>
2024-11-25 16:24:54 -0500

    working on database

--- server/sql/one.sql
+++ server/sql/one.sql
@@ -3,23 +3,23 @@ CREATE DATABASE personal_library;
 use rsonal_library;
 
 
-book_dictionary["Authors"] = authors
-book_dictionary["LC Classifications"] = lc_classifications
 book_dictionary["Publish Date"] = publish_date
 book_dictionary["Publisher"] = publisher
 book_dictionary["Subjects"] = subjects
 book_dictionary["Pagination"] = pagination
 book_dictionary["Info URL"] = info_url
-book_dictionary['Location'] = "A1"   
+book_dictionary['Location'] = "A1"    
 
 --Table for the books. 
 CREATE TABLE books
 (
   book_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(240) NOT NULL,
+  firstName VARCHAR(240) NOT NULL,
   lastName VARCHAR(240) NOT NULL,
+  LC_Classifications VARCHAR(240) NOT NULL,
+  Publish_Date DATE NOT NULL, 
   username VARCHAR(240) NOT NULL,
   email VARCHAR(240) NOT NULL,
-  
   PRIMARY KEY(user_id)
 );
