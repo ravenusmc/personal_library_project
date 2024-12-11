106a7cb27129faf8210adad079cda99e67f18f0b
ravenusmc <mcuddy77@gmail.com>
2024-12-11 16:57:24 -0500

    have three search methods working to search the database

--- server/app.py
+++ server/app.py
@@ -15,12 +15,17 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/getBooks', methods=['POST'])
 def get_Books_Data():
   if request.method == 'POST':
-      db_obj = Connection()
-      post_data = request.get_json()
-      query = post_data['query']
-      if post_data['type'] == 'Author':
-        author_results = db_obj.find_book_by_author(query)
-      return jsonify(author_results)
+    db_obj = Connection()
+    post_data = request.get_json()
+    query = post_data['query']
+    if post_data['type'] == 'Author':
+      results = db_obj.find_book_by_author(query)
+    elif post_data['type'] == 'Title':
+      print(query)
+      results = db_obj.find_book_by_title(query)
+    else: 
+      results = db_obj.find_books_by_subject(query)
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/db.py
+++ server/db.py
@@ -33,5 +33,43 @@ class Connection():
     except mysql.connector.Error as err:
             print(f"Error: {err}")
             return None
+  
+  def find_book_by_title(self, query):
+    try:
+        # Prepare the SQL query
+        sql_query = """
+            SELECT * FROM books 
+            WHERE Title LIKE %s
+        """
+        # Using parameterized queries to prevent SQL injection
+        like_query = f"%{query}%"
+        self.cursor.execute(sql_query, (like_query, ))
+        
+        # Fetch all matching rows
+        results = self.cursor.fetchone()
+        # Return the results
+        return results
+    except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
+  
+  def find_books_by_subject(self, query):
+    try:
+        # Prepare the SQL query
+        sql_query = """
+            SELECT * FROM books 
+            WHERE Subjects LIKE %s
+        """
+        # Using parameterized queries to prevent SQL injection
+        like_query = f"%{query}%"
+        self.cursor.execute(sql_query, (like_query, ))
+        
+        # Fetch all matching rows
+        results = self.cursor.fetchall()
+        # Return the results
+        return results
+    except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
     
 
